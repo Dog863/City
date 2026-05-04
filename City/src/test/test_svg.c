8f1e41aa7b2cc771971a55a0a1fb2a0904c72e54
Dog863 <davi.dognani.santos@uel.br>
2026-05-04 00:32:33 -0300

    Update test_svg.c

--- City/src/test/test_svg.c
+++ City/src/test/test_svg.c
@@ -6,17 +6,8 @@
 
 static char svg_file[] = "test_output.svg";
 
-void setUp(void) {
-    remove(svg_file);
-}
-
-void tearDown(void) {
-    svg_close();
-    remove(svg_file);
-}
-
-char* read_file(const char *filename) {
-    FILE *f = fopen(filename, "r");
+char* read_svg_file(void) {
+    FILE *f = fopen(svg_file, "r");
     if (!f) return NULL;
     
     fseek(f, 0, SEEK_END);
@@ -24,6 +15,10 @@ char* read_file(const char *filename) {
     fseek(f, 0, SEEK_SET);
     
     char *content = (char*)malloc(size + 1);
+    if (!content) {
+        fclose(f);
+        return NULL;
+    }
     fread(content, 1, size, f);
     content[size] = '\0';
     fclose(f);
@@ -31,9 +26,18 @@ char* read_file(const char *filename) {
     return content;
 }
 
+void setUp(void) {
+    remove(svg_file);
+}
+
+void tearDown(void) {
+    remove(svg_file);
+}
+
 void test_svg_init_creates_file(void) {
     svg_init(svg_file);
     svg_close();
+    
     FILE *f = fopen(svg_file, "r");
     TEST_ASSERT_NOT_NULL(f);
     fclose(f);
@@ -42,50 +46,55 @@ void test_svg_init_creates_file(void) {
 void test_svg_has_opening_tag(void) {
     svg_init(svg_file);
     svg_close();
-    char *content = read_file(svg_file);
+    
+    char *content = read_svg_file();
     TEST_ASSERT_NOT_NULL(content);
     TEST_ASSERT_TRUE(strstr(content, "<svg") != NULL);
     free(content);
 }
 
-void test_svg_rect_works(void) {
+void test_svg_rect_draws_correctly(void) {
     svg_init(svg_file);
-    svg_rect(10, 20, 100, 50, "red");
+    svg_rect(10.5, 20.5, 100.0, 50.0, "red");
     svg_close();
-    char *content = read_file(svg_file);
+    
+    char *content = read_svg_file();
     TEST_ASSERT_NOT_NULL(content);
     TEST_ASSERT_TRUE(strstr(content, "rect") != NULL);
     TEST_ASSERT_TRUE(strstr(content, "red") != NULL);
     free(content);
 }
 
-void test_svg_text_works(void) {
+void test_svg_text_draws_correctly(void) {
     svg_init(svg_file);
-    svg_text(30, 40, "Hello");
+    svg_text(30.0, 40.0, "Hello World");
     svg_close();
-    char *content = read_file(svg_file);
+    
+    char *content = read_svg_file();
     TEST_ASSERT_NOT_NULL(content);
     TEST_ASSERT_TRUE(strstr(content, "text") != NULL);
-    TEST_ASSERT_TRUE(strstr(content, "Hello") != NULL);
+    TEST_ASSERT_TRUE(strstr(content, "Hello World") != NULL);
     free(content);
 }
 
-void test_svg_line_works(void) {
+void test_svg_line_draws_correctly(void) {
     svg_init(svg_file);
     svg_line(0, 0, 100, 100, "black");
     svg_close();
-    char *content = read_file(svg_file);
+    
+    char *content = read_svg_file();
     TEST_ASSERT_NOT_NULL(content);
     TEST_ASSERT_TRUE(strstr(content, "line") != NULL);
     TEST_ASSERT_TRUE(strstr(content, "black") != NULL);
     free(content);
 }
 
-void test_svg_circle_works(void) {
+void test_svg_circle_draws_correctly(void) {
     svg_init(svg_file);
-    svg_circle(50, 60, 10, "blue");
+    svg_circle(50.0, 60.0, 10.0, "blue");
     svg_close();
-    char *content = read_file(svg_file);
+    
+    char *content = read_svg_file();
     TEST_ASSERT_NOT_NULL(content);
     TEST_ASSERT_TRUE(strstr(content, "circle") != NULL);
     TEST_ASSERT_TRUE(strstr(content, "blue") != NULL);
@@ -96,19 +105,25 @@ void test_svg_multiple_shapes(void) {
     svg_init(svg_file);
     svg_rect(0, 0, 10, 10, "red");
     svg_circle(5, 5, 2, "blue");
+    svg_line(0, 0, 10, 10, "green");
+    svg_text(2, 8, "Test");
     svg_close();
-    char *content = read_file(svg_file);
+    
+    char *content = read_svg_file();
     TEST_ASSERT_NOT_NULL(content);
     TEST_ASSERT_TRUE(strstr(content, "rect") != NULL);
     TEST_ASSERT_TRUE(strstr(content, "circle") != NULL);
+    TEST_ASSERT_TRUE(strstr(content, "line") != NULL);
+    TEST_ASSERT_TRUE(strstr(content, "text") != NULL);
     free(content);
 }
 
-void test_svg_closing_tag(void) {
+void test_svg_has_closing_tag(void) {
     svg_init(svg_file);
     svg_rect(0, 0, 10, 10, "red");
     svg_close();
-    char *content = read_file(svg_file);
+    
+    char *content = read_svg_file();
     TEST_ASSERT_NOT_NULL(content);
     TEST_ASSERT_TRUE(strstr(content, "</svg>") != NULL);
     free(content);
@@ -118,11 +133,11 @@ int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_svg_init_creates_file);
     RUN_TEST(test_svg_has_opening_tag);
-    RUN_TEST(test_svg_rect_works);
-    RUN_TEST(test_svg_text_works);
-    RUN_TEST(test_svg_line_works);
-    RUN_TEST(test_svg_circle_works);
+    RUN_TEST(test_svg_rect_draws_correctly);
+    RUN_TEST(test_svg_text_draws_correctly);
+    RUN_TEST(test_svg_line_draws_correctly);
+    RUN_TEST(test_svg_circle_draws_correctly);
     RUN_TEST(test_svg_multiple_shapes);
-    RUN_TEST(test_svg_closing_tag);
+    RUN_TEST(test_svg_has_closing_tag);
     return UNITY_END();
 }
