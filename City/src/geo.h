749d83ce62b8afdb0dcd318e60cab8f37e23476b
Dog863 <davi.dognani.santos@uel.br>
2026-07-06 11:39:31 -0300

    Delete City directory

--- City/src/geo.h
+++
@@ -1,7 +0,0 @@
-#ifndef GEO_H
-#define GEO_H
-
-void geo_insertQuadra(char *cep, double x, double y, double w, double h);
-void geo_setQuadraStyle(char *fill, char *stroke, char *sw);
-
-#endif
--- City/src/pessoa.h
+++
@@ -1,7 +0,0 @@
-#ifndef PESSOA_H
-#define PESSOA_H
-
-void pessoa_insert(char *cpf, char *nome, char *sobrenome, char sexo, char *nasc);
-void pessoa_morar(char *cpf, char *cep, char *face, int num, char *compl);
-
-#endif
