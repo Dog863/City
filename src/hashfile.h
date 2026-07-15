bcdf24666d3ef2f8a48582aee2a3f0061d0af354
Dog863 <davi.dognani.santos@uel.br>
2026-07-15 17:59:05 -0300

    Add hf_set_base_name function declaration

--- src/hashfile.h
+++ src/hashfile.h
@@ -4,6 +4,7 @@
 typedef void* Hash;
 
 void hf_set_output_dir(const char *dir);
+void hf_set_base_name(const char *name);
 Hash hf_create(char *filename, int size, int dataSize);
 int hf_insert(Hash h, char *key, void *data);
 void* hf_search(Hash h, char *key);
